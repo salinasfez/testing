33f9daa9884d258573c53033bf405430fa50e0c7
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-07-06 21:04:40 -0500

    uploading app to master

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>This is a test app for Ubuntu.</h4>
+    </div>
+  );
+}
+
+export default App;
